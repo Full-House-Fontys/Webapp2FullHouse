7df2a55efc30c577e1121f71e0a2913a889e561f
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-04-20 11:45:14 +0200

    database connection made

---
+++ MessageApplication/MessageApplication/SendMessageWeb.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MessageApplication
+{
+    public partial class SendMessageWeb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string dbConn = ConfigurationManager.ConnectionStrings["SQLCONNECTION"].ToString();
+            using (SqlConnection cn = new SqlConnection(dbConn))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT NAAM FROM MATERIAAL", cn);
+                cn.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Label1.Text += rdr[0].ToString();
+                }
+                cn.Close();
+            }
+        }
+    }
+}
